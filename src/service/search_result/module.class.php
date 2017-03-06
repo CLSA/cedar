46d77082d5faff756f4c0389518a80f705b72b35
Patrick Emond <emondpd@mcmaster.ca>
2017-03-06 15:07:02 -0500

    Participant search: only showing participants with recordings

---
+++ src/service/search_result/module.class.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * module.class.php
+ * 
+ * @author Patrick Emond <emondpd@mcmaster.ca>
+ * @filesource
+ */
+
+namespace cedar\service\search_result;
+use cenozo\lib, cenozo\log, cedar\util;
+
+/**
+ * Performs operations which effect how this module is used in a service
+ */
+class module extends \cenozo\service\search_result\module
+{
+  /**
+   * Extend parent method
+   */
+  public function prepare_read( $select, $modifier )
+  {
+    parent::prepare_read( $select, $modifier );
+
+    // restrict by sound file
+    $modifier->join(
+      'participant_sound_file_total',
+      'participant.id',
+      'participant_sound_file_total.participant_id'
+    );
+  }
+}
