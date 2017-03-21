3610c755fba885c126835466ea148b3658dea1d8
Patrick Emond <emondpd@mcmaster.ca>
2017-03-21 16:52:44 -0400

    Tweaking AFT/FAS interface when in readonly mode

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'cedar';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.0.0';
-$SETTINGS['general']['build'] = '900e54d';
+$SETTINGS['general']['build'] = '62ac2db';
 
 // the location of cedar internal path
 $SETTINGS['path']['APPLICATION'] = str_replace( '/settings.ini.php', '', __FILE__ );
