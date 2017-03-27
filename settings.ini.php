93f713f74f4acfdd91de3ff0753d3d4f787c1dc4
Patrick Emond <emondpd@mcmaster.ca>
2017-03-27 15:53:37 -0400

    Adding new cn-autofocus directive to modals

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'cedar';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.1.0';
-$SETTINGS['general']['build'] = 'd8c3bad';
+$SETTINGS['general']['build'] = '80f558b';
 
 // the location of cedar internal path
 $SETTINGS['path']['APPLICATION'] = str_replace( '/settings.ini.php', '', __FILE__ );
