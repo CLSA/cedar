0437931cbf29dcb33405e5fcaaee06aaa2cbb713
Patrick Emond <emondpd@mcmaster.ca>
2017-04-12 11:51:43 -0400

    Fixing missing uib- to angular-ui components

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'cedar';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.1.0';
-$SETTINGS['general']['build'] = '519c2cf';
+$SETTINGS['general']['build'] = '3d8d9d2';
 
 // the location of cedar internal path
 $SETTINGS['path']['APPLICATION'] = str_replace( '/settings.ini.php', '', __FILE__ );
