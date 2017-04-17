5e0c65c64ce01b640bb414011965190dd24e7911
Patrick Emond <emondpd@mcmaster.ca>
2017-04-17 10:30:37 -0400

    Only admins can set a user's cohort access

---
+++ web/app/user/module.extend.js
@@ -0,0 +1,28 @@
+// extend the framework's module
+define( [ cenozoApp.module( 'user' ).getFileUrl( 'module.js' ) ], function() {
+  'use strict';
+
+  var module = cenozoApp.module( 'user' );
+
+  // extend the view factory
+  cenozo.providers.decorator( 'CnUserViewFactory', [
+    '$delegate', 'CnSession',
+    function( $delegate, CnSession ) {
+      var instance = $delegate.instance;
+      $delegate.instance = function( parentModel, root ) {
+        var object = instance( parentModel, root );
+
+        // overrride cohort list's onDelete
+        object.deferred.promise.then( function() {
+          if( angular.isDefined( object.cohortModel ) ) {
+            object.cohortModel.getChooseEnabled = function() {
+              return 3 <= CnSession.role.tier && object.cohortModel.$$getChooseEnabled();
+            };
+          }
+        } );
+        return object;
+      };
+      return $delegate;
+    }
+  ] );
+} );
