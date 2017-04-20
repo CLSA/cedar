d768d83c0230bddcc93735496118051079faabb0
Patrick Emond <emondpd@mcmaster.ca>
2017-04-20 13:21:42 -0400

    Fixing sql bug

--- db/upgrade/2.1.0/update_word_test_entry_total.sql
+++ db/upgrade/2.1.0/update_word_test_entry_total.sql
@@ -8,7 +8,7 @@ CREATE PROCEDURE update_word_test_entry_total(IN proc_word_id INT(10) UNSIGNED)
 BEGIN
   -- get the number of test entries using the word
   REPLACE INTO word_test_entry_total( word_id, total )
-  SELECT word_id, IF( test_entry_has_word.word_id IS NULL, 0, COUNT(*) ) AS total
+  SELECT word.id, IF( test_entry_has_word.word_id IS NULL, 0, COUNT(*) ) AS total
   FROM word
   LEFT JOIN test_entry_has_word ON word.id = test_entry_has_word.word_id
   WHERE word.id = proc_word_id;
