bdfab19ecbd7ddda696ef97010052cb5c058eb8c
Patrick Emond <emondpd@mcmaster.ca>
2017-08-23 12:10:22 -0400

    Adding word::update_based_on_compound_words() method
    This has been added to automatically update a word's properties when compound
    words are added or removed.

--- src/database/compound.class.php
+++ src/database/compound.class.php
@@ -15,6 +15,30 @@ use cenozo\lib, cenozo\log, cedar\util;
 class compound extends \cenozo\database\has_rank
 {
   /**
+   * Extend parent method
+   */
+  public function delete()
+  {
+    $db_word = $this->get_word();
+
+    parent::delete();
+
+    // also update the base word's AFT and FAS status
+    $db_word->update_based_on_compound_words();
+  }
+
+  /**
+   * Extend parent method
+   */
+  public function save()
+  {
+    parent::save();
+
+    // also update the base word's AFT and FAS status
+    $this->get_word()->update_based_on_compound_words();
+  }
+
+  /**
    * The type of record which the record has a rank for.
    * @var string
    * @access protected
--- src/database/word.class.php
+++ src/database/word.class.php
@@ -46,6 +46,38 @@ class word extends \cenozo\database\record
   }
 
   /**
+   * Updates the word's data (AFT, FAS, misspelled) based on any compound words the base word may have.
+   * 
+   * If the word has no compound words this method does nothing
+   * @author Patrick Emond <emondpd@mcmaster.ca>
+   * @access public
+   */
+  public function update_based_on_compound_words()
+  {
+    $compound_sel = lib::create( 'database\select' );
+    $compound_sel->add_table_column( 'sub_word', 'aft' );
+    $compound_sel->add_table_column( 'sub_word', 'fas' );
+    $compound_mod = lib::create( 'database\modifier' );
+    $compound_mod->join( 'word', 'compound.sub_word_id', 'sub_word.id', '', 'sub_word' );
+
+    $compound_list = $this->get_compound_list( $compound_sel, $compound_mod );
+    if( 0 < count( $compound_list ) )
+    {
+      // words with compound words are always considered to be spelled correctly
+      $this->misspelled = false;
+      // invalid words should never have compound words, so assume "intrusion" until a compound word says otherwise
+      $this->aft = 'intrusion';
+      $this->fas = 'intrusion';
+      foreach( $compound_list as $compound )
+      {
+        if( 'primary' == $compound['aft'] ) $this->aft = 'primary';
+        if( 'primary' == $compound['fas'] ) $this->fas = 'primary';
+      }
+      $this->save();
+    }
+  }
+
+  /**
    * Returns the qualified value for the fas column based on the test type
    * 
    * This is necessary since the f-test has only f-words primary, a-test a-words and s-test s-words
@@ -59,9 +91,37 @@ class word extends \cenozo\database\record
     if( is_null( $db_test_type ) || 'fas' != $db_test_type->data_type )
       throw lib::create( 'exception\argument', 'db_test_type', $db_test_type, __METHOD__ );
 
-    return 'primary' == $this->fas ? (
-      strtolower( substr( $this->word, 0, 1 ) ) == strtolower( substr( $db_test_type->name, 0, 1 ) ) ?
-        'primary' : 'intrusion'
-      ) : $this->fas;
+    $fas = $this->fas;
+
+    if( 'primary' == $this->fas )
+    {
+      $test_type_letter = strtolower( substr( $db_test_type->name, 0, 1 ) );
+
+      $compound_sel = lib::create( 'database\select' );
+      $compound_sel->add_table_column( 'sub_word', 'word' );
+      $compound_mod = lib::create( 'database\modifier' );
+      $compound_mod->join( 'word', 'compound.sub_word_id', 'sub_word.id', '', 'sub_word' );
+      $compound_list = $this->get_compound_list( $compound_sel, $compound_mod );
+      if( 0 < count( $compound_list ) )
+      {
+        // primary if any compound words start with the test-type letter
+        $fas = 'intrusion';
+        foreach( $compound_list as $compound )
+        {
+          if( strtolower( substr( $compound['word'], 0, 1 ) ) == $test_type_letter )
+          {
+            $fas = 'primary';
+            break;
+          }
+        }
+      }
+      else
+      {
+        // primary if the word starts with the test-type letter
+        $fas = strtolower( substr( $this->word, 0, 1 ) ) == $test_type_letter ? 'primary' : 'intrusion';
+      }
+    }
+
+    return $fas;
   }
 }
