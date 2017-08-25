5ada237ac6c77d2edfe4f8a6a996596b41f84656
Patrick Emond <emondpd@mcmaster.ca>
2017-08-25 14:23:43 -0400

    forgot to update build number in last commit

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'cedar';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.1.0';
-$SETTINGS['general']['build'] = '08c754a';
+$SETTINGS['general']['build'] = '5383f83';
 
 // the location of cedar internal path
 $SETTINGS['path']['APPLICATION'] = str_replace( '/settings.ini.php', '', __FILE__ );
