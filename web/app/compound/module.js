30414dceb4256413c14196281d373f2d19d7b1c0
Patrick Emond <emondpd@mcmaster.ca>
2017-09-06 11:06:25 -0400

    Adding a "add word" extra operation to the add compound state

--- web/app/compound/module.js
+++ web/app/compound/module.js
@@ -44,6 +44,13 @@ define( function() {
     }
   } );
 
+  if( angular.isDefined( module.actions.add ) ) {
+    module.addExtraOperation( 'add', {
+      title: 'Add Word',
+      operation: function( $state, model ) { $state.go( 'word.add' ); }
+    } );
+  }
+
   /* ######################################################################################################## */
   cenozo.providers.directive( 'cnCompoundAdd', [
     'CnCompoundModelFactory',
@@ -76,8 +83,8 @@ define( function() {
 
   /* ######################################################################################################## */
   cenozo.providers.factory( 'CnCompoundAddFactory', [
-    'CnBaseAddFactory', 'CnHttpFactory',
-    function( CnBaseAddFactory, CnHttpFactory ) {
+    'CnBaseAddFactory', 'CnHttpFactory', 'CnSession',
+    function( CnBaseAddFactory, CnHttpFactory, CnSession ) {
       var object = function( parentModel ) {
         var self = this;
         CnBaseAddFactory.construct( this, parentModel );
@@ -99,6 +106,18 @@ define( function() {
                 self.currentParentLanguageId = response.data.language_id;
               } );
             }
+          } );
+        };
+
+        // replace some transition methods since it won't work if we've come from adding a new word
+        this.transitionOnCancel = function() {
+          this.parentModel.transitionToParentViewState( 'word', this.currentParentId );
+        };
+
+        this.transitionOnSave = function( record ) {
+          var self = this;
+          CnSession.workingTransition( function() {
+            self.parentModel.transitionToParentViewState( 'word', self.currentParentId );
           } );
         };
       };
