8e8bdd30ccb5878a9738123c16129a8a2a5c209d
Patrick Emond <emondpd@mcmaster.ca>
2017-10-20 11:15:28 -0400

    Fixing a bug which wasn't showing premat yes/no values correctly

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'cedar';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.1.1';
-$SETTINGS['general']['build'] = 'b3f6f23';
+$SETTINGS['general']['build'] = '7e9d794';
 
 // the location of cedar internal path
 $SETTINGS['path']['APPLICATION'] = str_replace( '/settings.ini.php', '', __FILE__ );
