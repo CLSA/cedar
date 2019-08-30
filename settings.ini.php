6577a890c450b6776cdd86fcea13f35b24a34da1
Patrick Emond <emondpd@mcmaster.ca>
2019-08-30 16:21:18 -0400

    Marking the start of version 2.5

--- settings.ini.php
+++ settings.ini.php
@@ -12,7 +12,7 @@ global $SETTINGS;
 // tagged version
 $SETTINGS['general']['application_name'] = 'cedar';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
-$SETTINGS['general']['version'] = '2.4';
+$SETTINGS['general']['version'] = '2.5';
 $SETTINGS['general']['build'] = '881bf53';
 
 // the location of cedar internal path
