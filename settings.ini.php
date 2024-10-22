bdabd650ccd8f4f73d71b2ccc4521f3ae1f70080
Patrick Emond <emondpd@mcmaster.ca>
2024-10-22 14:07:45 -0400

    Bumping to version 2.10

--- settings.ini.php
+++ settings.ini.php
@@ -12,7 +12,7 @@ global $SETTINGS;
 // tagged version
 $SETTINGS['general']['application_name'] = 'cedar';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
-$SETTINGS['general']['version'] = '2.9';
+$SETTINGS['general']['version'] = '2.10';
 $SETTINGS['general']['build'] = '9cbfdcb';
 
 // the location of cedar internal path
