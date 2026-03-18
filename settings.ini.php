91ea4e1d50e45e035eb3e4f654dcbcb07b49b2e8
Patrick Emond <emondpd@mcmaster.ca>
2026-03-18 12:13:20 -0400

    Bug: fixing problems with large lists (listing and choosing records)

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'cedar';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.10';
-$SETTINGS['general']['build'] = 'a704c8b';
+$SETTINGS['general']['build'] = 'b6e65d9';
 
 // the location of cedar internal path
 $SETTINGS['path']['APPLICATION'] = str_replace( '/settings.ini.php', '', __FILE__ );
