d878b08ee003e2cd177017d545257c25da7ed860
Patrick Emond <emondpd@mcmaster.ca>
2026-04-01 10:32:48 -0400

    v3: build version needed to be updated

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'cedar';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.10';
-$SETTINGS['general']['build'] = 'b6e65d9';
+$SETTINGS['general']['build'] = 'f6366cb';
 
 // the location of cedar internal path
 $SETTINGS['path']['APPLICATION'] = str_replace( '/settings.ini.php', '', __FILE__ );
