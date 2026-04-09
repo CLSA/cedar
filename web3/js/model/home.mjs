742ba86fc9a3e87440bcdffd5fb96d520f1e7a95
Patrick Emond <emondpd@mcmaster.ca>
2026-04-09 15:41:51 -0400

    v3: adding transcription list to typist home model

--- web3/js/model/home.mjs
+++ web3/js/model/home.mjs
@@ -1,5 +1,47 @@
+const { CN_base_element } = await import(`${CENOZO_URL}/js/element/base_element.mjs`);
+const { CN_session } = await import(`${CENOZO_URL}/js/session.mjs`);
 const classes = await import(`${CENOZO_URL}/js/model/home.mjs`);
 
 export class CN_home_model extends classes.CN_home_model {
-  // TODO: add transcription list to typist's home page
+  #transcription_model;
+
+  /**
+   * Extend parent method
+   */
+  update_element() {
+    super.update_element();
+    if ("typist" == CN_session.get("role", "name") && this.#transcription_model) {
+      this.get_element().querySelector("div[name=transcription-list]").replaceChildren(
+        this.#transcription_model.get_element()
+      );
+    }
+  }
+
+  /**
+   * Extend parent method
+   */
+  _create_element() {
+    const el = super._create_element();
+    if ("typist" == CN_session.get("role", "name")) {
+      el.querySelector("div.row").append(CN_base_element.html(
+        '<div name="transcription-list" class="mt-4"></div>'
+      ));
+    }
+    return el;
+  }
+
+  /**
+   * Extend parent method
+   */
+  async run() {
+    if ("typist" == CN_session.get("role", "name")) {
+      const transcription_module = CN_session.get_module("transcription");
+      await transcription_module.load_classes();
+      this.#transcription_model = transcription_module.create_model();
+      this.#transcription_model.configure(this.get_element().querySelector("div.row"), "list");
+      await this.#transcription_model.run();
+    }
+
+    await super.run();
+  }
 }
